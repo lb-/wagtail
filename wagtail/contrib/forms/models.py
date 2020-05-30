202f21c8addc58d5fcec4c97c1e9d3688e6db340
LB <mail@lb.ee>
2020-05-31 05:40:36 +1000

    fix linting

--- wagtail/contrib/forms/models.py
+++ wagtail/contrib/forms/models.py
@@ -14,8 +14,8 @@ from django.utils.translation import gettext_lazy as _
 
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.admin.mail import send_mail
-from wagtail.core.models import Orderable, Page
 from wagtail.contrib.forms.utils import get_field_clean_name
+from wagtail.core.models import Orderable, Page
 
 from .forms import FormBuilder, WagtailAdminFormPageForm
 
