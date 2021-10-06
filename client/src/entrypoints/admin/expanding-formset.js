c03fbd7c6ab1a1790ca1ad89598a451e64b5a26d
LB Johnston <mail@lb.ee>
2021-10-06 19:07:26 +1000

    WIP

--- client/src/entrypoints/admin/expanding-formset.js
+++ client/src/entrypoints/admin/expanding-formset.js
@@ -34,10 +34,34 @@ function buildExpandingFormset(prefix, opts = {}) {
 
   addButton.on('click', () => {
     if (addButton.hasClass('disabled')) return;
+    // what I really want is to avoid updating any nested script CONTENT
+    // but I do want to update the id of the nested script
+    // maybe?
     const newFormHtml = emptyFormTemplate
-      .replace(/__prefix__/g, formCount)
+      // .replace(/__prefix__/g, formCount)
       .replace(/<-(-*)\/script>/g, '<$1/script>');
+      // "(?:.*?(__prefix__)\s*?)"
+
+    /**
+     * I want to build a temporary DOM element
+     * document.createElement
+     * https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+     * Then I want to traverse the element, replacing content recursively until we get to a NEW
+     * django template script element, the content inside that should be replaced but
+     * with an additional __PREFIX__ kept I think
+     *
+     *
+     */
+
+    /**
+     *
+     * NAH NAH what I want is a smarter regex (lol)
+     * basically when there is a DOUBLE __prefix__ in the same attribute, we only want to update the first
+     * OR we just replace the prefix
+     */
+
     formContainer.append(newFormHtml);
+    console.log('add item', { formCount, formContainer, prefix, newFormHtml });
     if (opts.onAdd) opts.onAdd(formCount);
     if (opts.onInit) opts.onInit(formCount);
 
