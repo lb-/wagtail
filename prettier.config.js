a0b5780049fd90fe8e78a30501ba2090adfafd9f
Thibaud Colas <thibaudcolas@gmail.com>
2022-01-31 15:19:19 +0000

    Add Prettier configuration

---
+++ prettier.config.js
@@ -0,0 +1,20 @@
+/**
+ * See https://prettier.io/docs/en/options.html.
+ */
+module.exports = {
+  printWidth: 80,
+  tabWidth: 4,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'auto',
+};
