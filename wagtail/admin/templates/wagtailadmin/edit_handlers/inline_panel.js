486b52d4ac48800ce153da1a618dda8fa0d9979f
LB Johnston <mail@lb.ee>
2022-02-03 22:36:56 +1000

    revise Python template syntax usage to allow for linting of InlinePanel calls

--- wagtail/admin/templates/wagtailadmin/edit_handlers/inline_panel.js
+++ wagtail/admin/templates/wagtailadmin/edit_handlers/inline_panel.js
@@ -1,17 +1,17 @@
-{% load l10n %}
-{% load wagtailadmin_tags %}
-(function() {
-    var panel = InlinePanel({
-        formsetPrefix: "id_{{ self.formset.prefix }}",
-        emptyChildFormPrefix: "{{ self.empty_child.form.prefix }}",
-        canOrder: {% if can_order %}true{% else %}false{% endif %},
-        maxForms: {{ self.formset.max_num|unlocalize }}
-    });
+/* {% load l10n %} */
+/* global InlinePanel */
+(function () {
+  var panel = InlinePanel({
+    formsetPrefix: 'id_{{ self.formset.prefix }}',
+    emptyChildFormPrefix: '{{ self.empty_child.form.prefix }}',
+    canOrder: !!'{% if can_order %}true{% endif %}',
+    maxForms: Number('{{ self.formset.max_num|unlocalize }}'),
+  });
 
-    {% for child in self.children %}
-        panel.initChildControls("{{ child.form.prefix }}");
-    {% endfor %}
-    panel.setHasContent();
-    panel.updateMoveButtonDisabledStates();
-    panel.updateAddButtonState();
+  // {% for child in self.children %}
+  panel.initChildControls('{{ child.form.prefix }}');
+  // {% endfor %}
+  panel.setHasContent();
+  panel.updateMoveButtonDisabledStates();
+  panel.updateAddButtonState();
 })();
--- wagtail/contrib/search_promotions/templates/wagtailsearchpromotions/includes/searchpromotions_formset.js
+++ wagtail/contrib/search_promotions/templates/wagtailsearchpromotions/includes/searchpromotions_formset.js
@@ -1,13 +1,14 @@
-$(function() {
-    var panel = InlinePanel({
-        formsetPrefix: "id_{{ formset.prefix }}",
-        emptyChildFormPrefix: "{{ formset.empty_form.prefix }}",
-        canOrder: true
-    });
+/* global InlinePanel */
+$(function () {
+  var panel = InlinePanel({
+    formsetPrefix: 'id_{{ formset.prefix }}',
+    emptyChildFormPrefix: '{{ formset.empty_form.prefix }}',
+    canOrder: true,
+  });
 
-    {% for form in formset.forms %}
-        panel.initChildControls('{{ formset.prefix }}-{{ forloop.counter0 }}');
-    {% endfor %}
+  // {% for form in formset.forms %}
+  panel.initChildControls('{{ formset.prefix }}-{{ forloop.counter0 }}');
+  // {% endfor %}
 
-    panel.updateMoveButtonDisabledStates();
+  panel.updateMoveButtonDisabledStates();
 });
